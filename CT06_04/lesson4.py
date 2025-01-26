c74d8828c61b1fa56793e1db990570eaa77bc534
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-01-26 09:16:13 +0000

    Jan 26, 2025, 9:16 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -5,5 +5,5 @@ green=3
 numred=input("how many red plates of sushi did shrek eat?")
 numblue=input("how many blue plates of sushi did shrek eat?")
 numgreen=input("how many green plates of sushi did shrek eat?")
-total=(3*red+5*blue+4*green)
+total=(numred*red+5*blue+4*green)
 print("shrek went and buy sushi that costs $"+ str(total))
