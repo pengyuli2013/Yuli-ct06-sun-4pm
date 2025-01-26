2f3dfe9bcab47112e0805b19f5cd33a495acfd28
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-01-26 08:50:14 +0000

    Jan 26, 2025, 8:50 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,6 +1,7 @@
-# print("Hello from lesson 4")
-# red=1
-# blue=2
-# green=3
-# total=(3*red+5*blue+4*green)
-# print(total)
+print("Hello from lesson 4")
+red=1
+blue=2
+green=3
+total=(3*red+5*blue+4*green)
+print(total)
+var
