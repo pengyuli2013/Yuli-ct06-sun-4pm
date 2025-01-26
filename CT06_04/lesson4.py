72fec19fca1b4294292fa6b575a5522b1cfcf78d
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-01-26 09:13:56 +0000

    Jan 26, 2025, 9:13 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -2,6 +2,6 @@ print("Hello from lesson 4")
 red=1
 blue=2
 green=3
-input
+input("how many blue plates of sushi did shrek eat?")
 total=(3*red+5*blue+4*green)
 print("shrek went and buy sushi that costs $"+ str(total))
