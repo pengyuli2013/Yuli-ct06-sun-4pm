97747c5a6fbbff4d734cba1002e415e18e66676b
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-02-09 08:58:00 +0000

    Feb 9, 2025, 8:58 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -3,11 +3,11 @@
 # Name=input("what is ur name")
 # thingtheywant=input("what do you want")
 # print("Happy "+Age+"th birthday "+Name+" ! "+thingtheywant)
-for i in range(100):
-    print("I love chicken rice")
-for i in range(100):
-    print("i like cake")
-    print("gimme more")
+# for i in range(100):
+#     print("I love chicken rice")
+# for i in range(100):
+#     print("i like cake")
+#     print("gimme more")
 var=input("How many numbers do you want me to print")
 for i in range(1-(var-1)):
     print(i)
