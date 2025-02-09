b67998dc755af2f4977cc05ec23bcc339a39649e
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-02-09 08:49:24 +0000

    Feb 9, 2025, 8:49 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,7 +1,7 @@
-print("Hello from lesson 5")
-Age=input("what is your age")
-Name=input("what is ur name")
-thingtheywant=input("what do you want")
-print("Happy "+Age+"th birthday "+Name+" ! "+thingtheywant)
+# print("Hello from lesson 5")
+# Age=input("what is your age")
+# Name=input("what is ur name")
+# thingtheywant=input("what do you want")
+# print("Happy "+Age+"th birthday "+Name+" ! "+thingtheywant)
 for i in range(100):
     print("I love chiken rice")
