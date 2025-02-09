0c8ae4e921440443c0038988873c6bfde6593c36
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-02-09 08:32:48 +0000

    Feb 9, 2025, 8:32 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,5 +1,6 @@
 print("Hello from lesson 5")
 Age=input("what is your age")
 Name=input("what is ur name")
-thingtheywant=intput("what do you want")
+thingtheywant=in9
+put("what do you want")
 print("Happy "+Age+"th birthday "+Name+" ! "+thingtheywant)
