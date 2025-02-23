7d46ca0e1d5e48f3f50db296fb45a1851133cef7
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-02-23 08:31:55 +0000

    Feb 23, 2025, 8:31 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,13 +1,13 @@
-score_one = 80
-score_two = 90
-score_three = 75
+# score_one = 80
+# score_two = 90
+# score_three = 75
 
-total = score_one + score_two + score_three
+# total = score_one + score_two + score_three
 
-average_score =str( total / 3)
+# average_score =str( total / 3)
 
-student_name = "Alex"
+# student_name = "Alex"
 
-print("Average score for " + student_name + " is: " + average_score)
+# print("Average score for " + student_name + " is: " + average_score)
 for i in range(1,11,1):
     print:(i)
