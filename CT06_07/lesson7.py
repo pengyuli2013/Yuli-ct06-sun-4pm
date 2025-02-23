045cfdbf89c9205bd8c1b01feb9d64fd29d6e5bb
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-02-23 09:35:54 +0000

    Feb 23, 2025, 9:35 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -27,9 +27,9 @@
 n=int(input("what num for times table"))
 num=1
 for i in range(1,13):
+    print(str(n)+"x"+str(num)+"="+str(print(n*num)))
     print(n*num)
     num=num+1
-    print(str(n)+"x"+str(num)+"=")
 
 
 
