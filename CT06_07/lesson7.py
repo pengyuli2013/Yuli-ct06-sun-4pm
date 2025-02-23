94016ada5ef9f531e59c41a6eb846413818c70de
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-02-23 08:12:27 +0000

    Feb 23, 2025, 8:12 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,1 +1,11 @@
-# print("Hello from lesson 
+score_one = 80
+score_two = 90
+score_three = 75
+
+total = score_one + score_two + score_three
+
+average_score = totl / 3
+
+student_name = "Alex"
+
+print("Average score for " + student_name + " is: " + average_score)
