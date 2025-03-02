0a697b5187904b5935e35678210e2595f2577cef
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-03-02 08:20:06 +0000

    Mar 2, 2025, 8:20 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,3 +1,6 @@
 print("Hello from lesson 8")
 num1=int(input("what is da first num"))
+num2=int(input("what is da first num"))
+num3=int(input("what is da first num"))
+num1=int(input("what is da first num"))
 num1=int(input("what is da first num"))
