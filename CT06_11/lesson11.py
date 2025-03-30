922c0f42738844ca695253a0cbd915a50439e050
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-03-30 08:30:45 +0000

    Mar 30, 2025, 8:30 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,11 +1,11 @@
-ans=int(input("what is the price of the item?"))
-if ans <= 5:
-    print("sounds good" )
-elif ans <= 50:
-    print("u need dis")
-elif ans <= 500:
-    print("u rob form bank?")
-elif ans > 500:
-    print("dont even tink about dis")
-else:
-    print("skibidi")
+# ans=int(input("what is the price of the item?"))
+# if ans <= 5:
+#     print("sounds good" )
+# elif ans <= 50:
+#     print("u need dis")
+# elif ans <= 500:
+#     print("u rob form bank?")
+# elif ans > 500:
+#     print("dont even tink about dis")
+# else:
+#     print("skibidi")
