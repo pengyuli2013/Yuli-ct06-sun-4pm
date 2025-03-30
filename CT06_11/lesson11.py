712bcd50e5d772e361504199aeec2e5ec88b9be6
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-03-30 09:26:26 +0000

    Mar 30, 2025, 9:26 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -30,7 +30,7 @@ var=input("username?")
 var2=input("passvord?")
 if var=="john123" and var2=="pw123":
     print("access granted")
-elif var!="john123" or var2!="pw123":
+elif var=="john123" or var2=="pw123":
     print("jjjjjjjjjjjj")
 else:
     print("access denied")
