5c9d7440aed2c26fa93f732772bd3c468588b76e
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-03-30 08:17:15 +0000

    Mar 30, 2025, 8:17 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -2,7 +2,7 @@ ans=int(input("what is the price of the item?"))
 if ans <= 5:
     print("sounds good" )
 elif ans <= 50:
-    "u need dis"
+    print("u need dis")
 elif ans <= 500:
     "u rob form bank?"
 elif ans > 500:
