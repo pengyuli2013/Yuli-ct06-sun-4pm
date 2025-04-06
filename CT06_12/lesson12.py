960735185bb465bb214c84c7d5f159ecd75ef9bd
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-04-06 08:14:11 +0000

    Apr 6, 2025, 8:14 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,1 +1,12 @@
-print("Hello from lesson 12")
+# Lesson 12 - While Loops
+
+# Recap 1: Multiples of 3 and 5
+# Create a program to check if a number is both divisible by 3
+# and 5.
+
+# 1. Ask the user to input a number
+# 2. Using the '%' operator, check if the number is both a
+#    multiple of 3 and 5:
+#     If true, print "The number is divisible by 3 and 5!"
+# 3. Else, print "The number is not divisible by 3 and 5"
+
