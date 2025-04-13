c3fbad974d70be97a377d53546b9852380de6679
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-04-13 09:12:27 +0000

    Apr 13, 2025, 9:12 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,1 +1,16 @@
-print("Hello from lesson 13")
+groceries = [
+    "Apples",
+    "Bread",
+    "Carrots",
+    "Dates",
+    "Eggs",
+    "Flour",
+    "Grapes",
+    "Honey"
+]
+# groceries.insert(1,"bananana")
+# groceries.append("ice")
+# groceries[7]="herbs"
+# print(groceries)
+# groceries.insert(1,"bananana")
+# del
