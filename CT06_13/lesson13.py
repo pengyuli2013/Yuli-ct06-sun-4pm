43be639da0efe2337d585bbf47eefa13dfc7040a
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-04-13 09:21:16 +0000

    Apr 13, 2025, 9:21 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -18,7 +18,7 @@ for grocery in groceries:
     if grocery=="Apples":
         print("Apples: I need five of These")
     elif grocery=="Carrots":
-        print("Carrots: I need five of These")
+        print("Carrots: I need three of These")
     
 
     print(grocery)
