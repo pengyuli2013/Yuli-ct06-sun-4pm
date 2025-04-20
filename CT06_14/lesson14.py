f6e17e0e8f218a6bf3a1fa009a73abe087935406
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-04-20 09:11:16 +0000

    Apr 20, 2025, 9:11 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -6,6 +6,85 @@ turtle.shape("square")
 t.fillcolor("orange")
 window=turtle.Screen()
 window.setup(width=600,height=400)
+## Task 3: Drawing
+# Given the number of sides and each interior angle, draw each of the
+# following shapes using a loop and the following functions:
+#     .seth()
+#     .up()
+#     .down()
+#     .forward()
+#     .backward()
+#     .left()
+#     .right()
+
+# **Task 3a**: Draw a line
+# Number of sides: 1
+# Interior angle: NA
+
+# **Task 3b**: Draw a triangle
+# Number of sides: 3
+# Interior angle: 120
+
+# **Task 3c**: Draw a square
+# Number of sides: 4
+# Interior angle: 90
+
+# **Task 3d**: Draw a pentagon
+# Number of sides: 5
+# Interior angle: 72
+
+# **Task 3e**: Draw a hexagon
+# Number of sides: 6
+# Interior angle: 60
+
+# **Task 3f**: Draw a circle
+# Number of sides: 360
+# Interior angle: 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
