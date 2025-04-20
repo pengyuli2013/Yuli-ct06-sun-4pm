7fc5374632272aeee672b77204416119a471b63a
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-04-20 08:58:44 +0000

    Apr 20, 2025, 8:58 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -2,7 +2,7 @@ print("Hello from lesson 15")
 import turtle
 t=turtle.Turtle()
 turtle.shape(turtle)
-turtle.shape
+turtle.fillcolour()
 window=turtle.Screen()
 window.setup(width=600,height=400)
 window.mainloop()
