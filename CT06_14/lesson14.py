07e7412bc883c4baa5840e420c19574f5df2289d
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-04-27 08:56:44 +0000

    Apr 27, 2025, 8:56 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -36,7 +36,7 @@ t.fillcolor("orange")
 # for i in range(360):
 #     t.forward(1)
 #     t.left(1)
-t.goto(-300)
+t.goto(-300,0)
 # # **Task 3c**: Draw a square
 # # Number of sides: 4
 # # Interior angle: 90
