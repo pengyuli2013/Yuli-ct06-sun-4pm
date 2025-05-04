69ea9c475e59ab177f8f9071c622cf69b90cb2a9
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-05-04 08:27:28 +0000

    May 4, 2025, 8:27 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,15 +1,18 @@
-var=10
-while True:
-    print(var)
-    var=var+10
-    if var>200:
-        break
-#task 1
+# var=10
+# while True:
+#     print(var)
+#     var=var+10
+#     if var>200:
+#         break
+# #task 1
 
-password="superpass123"
-ans=input("password?")
-if ans==password:
-    print("Access Granted")
-else:
-    print("Access denied")
+# password="superpass123"
+# ans=input("password?")
+# if ans==password:
+#     print("Access Granted")
+# else:
+#     print("Access denied")
 #task 2
+
+
+question2
