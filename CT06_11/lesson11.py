80816893012806641990fe5cfff85cecdba65bcc
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-05-04 08:17:47 +0000

    May 4, 2025, 8:17 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,36 +1,7 @@
-# ans=int(input("what is the price of the item?"))
-# if ans <= 5:
-#     print("sounds good" )
-# elif ans <= 50:
-#     print("u need dis")
-# elif ans <= 500:
-#     print("u rob form bank?")
-# elif ans > 500:
-#     print("dont even tink about dis")
-# else:
-#     print("skibidi")
-# num=int(input("numba"))
-# if num%3==0 and num%7==0:
-#     print("saihdfliushfli")
-# age=int(input("age"))
-# if age<12 or age>65:
-#     print("15")
-# else:
-#     print("20")
-# colour=(input("colour")
-# if not colour=="green":
-#     print("try again")
-# food=input("buger?")
-# food1=input("fwies")
-# drink=input("dwink")
-# if food=="yes" and food1=="yes":
-#     if not drink=="yes":
-#         print("thirsty")
-var=input("username?")
-var2=input("passvord?")
-if var=="john123" and var2=="pw123":
-    print("access granted")
-elif var=="john123" or var2=="pw123":
-    print("jjjjjjjjjjjj")
-else:
-    print("access denied")
+var=10
+while True:
+    print(var)
+    var=var+10
+    if var>200:
+        break
+    
