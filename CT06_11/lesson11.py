916de5d188e30fe691336c5db5a0a66668e55521
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-05-04 08:27:15 +0000

    May 4, 2025, 8:27 AM

---
+++ CT06_11/lesson11.py
@@ -0,0 +1,15 @@
+var=10
+while True:
+    print(var)
+    var=var+10
+    if var>200:
+        break
+#task 1
+
+password="superpass123"
+ans=input("password?")
+if ans==password:
+    print("Access Granted")
+else:
+    print("Access denied")
+#task 2
