54d08b7dc16f5527f1d2bcbcc4b437e4a83119bc
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-05-11 08:16:07 +0000

    May 11, 2025, 8:16 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -1,46 +1,46 @@
-# Lesson 15 - Functions
+# # Lesson 15 - Functions
 
-## Recap 1: Turtle drawing
-**Recap 1a**:
-Using the 'turtle' library, create a 200x200 window
+# ## Recap 1: Turtle drawing
+# **Recap 1a**:
+# Using the 'turtle' library, create a 200x200 window
 
-1. Import 'turtle' library
-2. Using '.setup()', create a window 200 in width and 200 in height
-3. Add a '.mainloop()' function to keep the window open
+# 1. Import 'turtle' library
+# 2. Using '.setup()', create a window 200 in width and 200 in height
+# 3. Add a '.mainloop()' function to keep the window open
 
-**Recap 1b**:
-Modify your previous code to create a blue arrow as your turtle.
+# **Recap 1b**:
+# Modify your previous code to create a blue arrow as your turtle.
 
-1. Create a turtle called "artist" using 'turtle.Turtle()'
-2. Using '.shape()', change the turtle shape to "arrow"
-3. Using '.color()', change the turtle color to "blue"
+# 1. Create a turtle called "artist" using 'turtle.Turtle()'
+# 2. Using '.shape()', change the turtle shape to "arrow"
+# 3. Using '.color()', change the turtle color to "blue"
 
-**Recap 1c**:
-Modify your previous code to draw the shape of a compass rose (shown
-on screen)
+# **Recap 1c**:
+# Modify your previous code to draw the shape of a compass rose (shown
+# on screen)
 
-1. Use '.penup()' and position turtle to (0, 0) using '.goto()'
-2. Point turtle towards North ("90") using '.seth()'
-3. Use '.pendown()' and draw "60" towards North
-4. Using '.right()', turn turtle 90 degrees to the right 
-5. Using a 'for' loop, draw a circle by moving 1 step each time
-   before turning 1 degree to the right for 360 times.
+# 1. Use '.penup()' and position turtle to (0, 0) using '.goto()'
+# 2. Point turtle towards North ("90") using '.seth()'
+# 3. Use '.pendown()' and draw "60" towards North
+# 4. Using '.right()', turn turtle 90 degrees to the right 
+# 5. Using a 'for' loop, draw a circle by moving 1 step each time
+#    before turning 1 degree to the right for 360 times.
 
-**Recap 1d**:
-Modify your previous code to print the position of your turtle at the
-end of the drawing.
+# **Recap 1d**:
+# Modify your previous code to print the position of your turtle at the
+# end of the drawing.
 
-1. Using '.xcor()' and 'ycor()', print onto the console your turtle's
-   last coordinates in the following format:
+# 1. Using '.xcor()' and 'ycor()', print onto the console your turtle's
+#    last coordinates in the following format:
 
-   "Current turtle position: <x>, <y>"
+#    "Current turtle position: <x>, <y>"
 
 print("Hello from lesson 14")
 import turtle
 window=turtle.Screen()
 window.setup(200,200)
 turtle.Turtle()
-turtle.shape("arrow")
+turtle.shape(arrow)
 
 
 
