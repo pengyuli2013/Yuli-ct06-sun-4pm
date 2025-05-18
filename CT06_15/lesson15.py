a26373f397b3aa1b41f372120b32655001a792a9
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-05-18 09:19:38 +0000

    May 18, 2025, 9:19 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -112,6 +112,9 @@ def move_ball(ball, dx,dy):
 def check_x(ball,screenwidth):
     if ball.xcor()>(screenWidth/2)or ball.xcor() < (-screenWidth/2):
         return True
+def check_y(ball,screenwidth):
+    if ball.xcor()>(screenWidth/2)or ball.xcor() < (-screenWidth/2):
+        return True
 dx=2
 dy=2
 while True:
